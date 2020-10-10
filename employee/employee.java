6332672f207079a3260fdde8703fe7cad66b8dcd
naimanayyu123 <70109948+naimanayyu123@users.noreply.github.com>
2020-10-10 12:16:30 +0530

    Add files via upload

---
+++ employee/employee.java
@@ -0,0 +1,12 @@
+package com.employee;
+
+public class employee {
+	String name ;
+	int age ;
+	String city;
+
+		// TODO Auto-generated method stub
+        
+	}
+
+
---
+++ employee/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Waheda
+ *
+ */
+package com.employee;
